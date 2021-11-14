834c0eaa4fdebda2a0cdac5b94f27af1a8dd10dd
Jerry Toscano <Jerry.Toscano@strongmind.com>
2021-11-14 01:16:02 -0700

    started DataCache tests

---
+++ Cella.Core.Tests/DataCacheTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Cella.Core.Pages;
+using Xunit;
+
+namespace Cella.Core.Tests
+{
+    public class DataCacheTests
+    {
+        private class MockPage : Page
+        {
+
+        }
+
+        private static readonly DataCache.Options options = 
+            new(
+            10000, 
+            20000, 
+            50, 
+            100, 
+            1000, 
+            16, 
+            2000);
+
+        [Fact]
+        public async Task GetsSamePage()
+        {
+            BufferPool bp = new(512);
+            await using DataCache dc = new(bp, DataCacheTests.options);
+            MockPage mp = new();
+            FullPageId id = new();
+            await dc.GetPageAsync(id, () => mp);
+            Assert.Equal(mp, await dc.GetPageAsync(id, () => new MockPage()));
+        }
+    }
+}
