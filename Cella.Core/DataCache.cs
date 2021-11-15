b4e9c7c7307eb11459252f549ea3df17202193dc
Jerry Toscano <Jerry.Toscano@strongmind.com>
2021-11-14 22:14:57 -0700

    fixing cache

--- Cella.Core/DataCache.cs
+++ Cella.Core/DataCache.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using Cella.Core.Pages;
 
 namespace Cella.Core;
@@ -24,28 +25,23 @@ public sealed class DataCache : IAsyncDisposable
     private LinkedListNode<Slot>? nextNode;
     private readonly Task lazyWriter;
     private bool shutdown;
-    private readonly ConcurrentQueue<Slot> dirtyPages = new();
-    private readonly Task flusher;
-    private readonly ManualResetEventSlim dirtyAvailable = new();
+    private bool generationFlag;
 
     public DataCache(BufferPool bufferPool, Options options)
     {
         this.bufferPool = bufferPool;
         this.options = options;
         this.lazyWriter = Task.Run(this.LazyWriterWorkerAsync);
-        this.flusher = Task.Run(this.FlusherAsync);
     }
 
     public async ValueTask DisposeAsync()
     {
         this.shutdown = true;
-        this.dirtyAvailable.Set();
         await this.lazyWriter;
         foreach (var freeBuffer in freeBuffers)
             freeBuffer.Dispose();
         foreach (var slot in allocated)
             slot.Buffer.Dispose();
-        await this.flusher;
     }
 
     private async Task LazyWriterWorkerAsync()
@@ -74,26 +70,15 @@ public sealed class DataCache : IAsyncDisposable
                 if (now - currentSlot.LastAccess <= this.options.CorrelatedReferencePeriod
                     || currentSlot.UncorrelatedAccess2 < this.options.MaxSecondaryCorrelationPeriod)
                     continue;
-                var currentPage = currentSlot.Page;
                 lock (currentSlot)
                 {
                     if (now - currentSlot.LastAccess <= this.options.CorrelatedReferencePeriod)
                         continue;
                     currentSlot.Invalid = true;
-                    if (currentPage.IsDirty)
-                    {
-                        currentSlot.WriteLock = new();
-                        Monitor.Enter(currentSlot.WriteLock);
-                        this.dirtyPages.Enqueue(currentSlot);
-                        this.dirtyAvailable.Set();
-                    }
+                    if (currentSlot.Page.IsDirty)
+                        this.FlushPage(currentNode, true);
                     else
-                    {
-                        cache.TryRemove(currentPage.FullPageId, out _);
-                        lock (allocated)
-                            allocated.Remove(currentNode);
-                        freeBuffers.Enqueue(currentSlot.Buffer);
-                    }
+                        DeallocateNode(currentNode);
                 }
             }
 
@@ -101,20 +86,27 @@ public sealed class DataCache : IAsyncDisposable
         } while (!shutdown);
     }
 
-    private async Task FlusherAsync()
+    private void DeallocateNode(LinkedListNode<Slot> node)
     {
-        for (; ; )
+        this.cache.TryRemove(node.Value.Page.FullPageId, out _);
+        lock (this.allocated)
+            this.allocated.Remove(node);
+        this.freeBuffers.Enqueue(node.Value.Buffer);
+    }
+
+    private void FlushPage(LinkedListNode<Slot> node, bool removeOnWrite)
+    {
+        var slot = node.Value;
+        slot.WriteLock ??= new();
+        Monitor.Enter(slot.WriteLock);
+        slot.GenerationFlag = !slot.GenerationFlag;
+        Task.Run(async () =>
         {
-            this.dirtyAvailable.Wait();
-            this.dirtyAvailable.Reset();
-            if (this.shutdown)
-                return;
-            while (this.dirtyPages.TryDequeue(out var slot))
-            {
-                await slot.Page.FlushAsync();
-                Monitor.Exit(slot.WriteLock!);
-            }
-        }
+            await slot.Page.FlushAsync();
+            Monitor.Exit(slot.WriteLock!);
+            if (removeOnWrite)
+                this.DeallocateNode(node);
+        });
     }
 
     public async ValueTask<Page> GetPageAsync(FullPageId pageId, Func<Page> loader)
@@ -123,29 +115,72 @@ public sealed class DataCache : IAsyncDisposable
         if (!this.cache.TryGetValue(pageId, out var slot))
         {
             BufferPool.Buffer buffer;
-            while (!freeBuffers.TryDequeue(out buffer))
+            while (!freeBuffers.TryDequeue(out buffer)) // learn from this
                 await Task.Delay(this.options.LazyWriterInterval);
-            slot = new(buffer, loader()) { UncorrelatedAccess1 = ticks };
+            slot = new(buffer, loader(), ticks, this.generationFlag);
             lock (this.allocated)
                 this.allocated.AddLast(slot);
             this.cache[pageId] = slot;
         }
-        else if (ticks - slot.LastAccess > this.options.CorrelatedReferencePeriod)
-        {
-            slot.UncorrelatedAccess2 = slot.UncorrelatedAccess1 + (slot.LastAccess - slot.UncorrelatedAccess1);
-            slot.UncorrelatedAccess1 = ticks;
-        }
+        else 
+            slot.UpdateAccess(ticks, ticks - slot.LastAccess <= this.options.CorrelatedReferencePeriod);
 
-        slot.LastAccess = ticks;
         return slot.Page;
     }
 
-    private record Slot(BufferPool.Buffer Buffer, Page Page)
+    // call every min if 10MB of log used
+    public void CheckPoint()
     {
-        public long LastAccess { get; set; }
+        var currentFlag = this.generationFlag;
+        this.generationFlag = !currentFlag;
+        lock (this.allocated)
+            foreach (var slot in this.allocated)
+                slot.GenerationFlag = currentFlag;
+
+        LinkedListNode<Slot> node;
+        for (;;)
+        {
+            var tryNode = this.allocated.First;
+            if (tryNode == null)
+            {
+                if (this.allocated.Count == 0)
+                    return; // nothing to do
+                continue;
+            }
+
+            node = tryNode;
+            var slot = node.Value;
+            Monitor.Enter(slot);
+            if (slot.Invalid)
+            {
+                Monitor.Exit(slot);
+                continue;
+            }
+        }
+    }
+
+    private record struct Slot(BufferPool.Buffer Buffer, Page Page)
+    {
+        public bool GenerationFlag { get; set; }
+        public long LastAccess { get; private set; }
         public long UncorrelatedAccess1 { get; set; }
-        public long UncorrelatedAccess2 { get; set; }
+        public long UncorrelatedAccess2 { get; private set; }
         public bool Invalid { get; set; }
         public object? WriteLock { get; set; }
+        public Slot(BufferPool.Buffer buffer, Page page, long ticks, bool generationFlag) : this(buffer, page)
+        {
+            this.GenerationFlag = generationFlag;
+            this.LastAccess = this.UncorrelatedAccess1 = ticks;
+        }
+
+        public void UpdateAccess(long ticks, bool isCorrelated)
+        {
+            this.LastAccess = ticks;
+            if (isCorrelated)
+                return;
+
+            this.UncorrelatedAccess2 = this.UncorrelatedAccess1 + (this.LastAccess - this.UncorrelatedAccess1);
+            this.UncorrelatedAccess1 = ticks;
+        }
     }
 }
