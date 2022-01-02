8a94faee4b05e6d5a2cda23ac66bfdcc7595dc8f
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-01-02 16:24:49 -0700

    Variable string data

--- Cella.Core.Tests/DataTypeTests.cs
+++ Cella.Core.Tests/DataTypeTests.cs
@@ -12,9 +12,9 @@ public class DataTypeTests
     [InlineData(3, "test", "tes")]
     [InlineData(5, "test¿", "test\0")]
     [InlineData(5, "¿¿¿¿¿", "¿¿\0")]
-    public void StringTests(int length, string input, string output)
+    public void FixedStringTests(int length, string input, string output)
     {
-        StringDataType dt = new(length);
+        FixedStringDataType dt = new(length);
         var data = new byte[length];
         dt.Write(data, ref input);
         Assert.Equal(output, dt.Read(data));
--- Cella.Core/DataTypes/DataType.cs
+++ Cella.Core/DataTypes/DataType.cs
@@ -5,15 +5,86 @@ using System.Text;
 
 public abstract class DataType<T>
 {
-    public abstract T Read(ReadOnlySpan<byte> data);
-    public abstract void Write(Span<byte> data, ref T value);
-    public abstract int Length { get; }
+    public abstract int MaxLength { get; }
+
+    protected static (byte[] Bytes, int Characters) ConvertString(int maxLength, string value)
+    {
+        var chars = value.ToCharArray();
+        var stringLength = Math.Min(chars.Length, maxLength);
+        var byteCount = Encoding.UTF8.GetByteCount(chars, 0, stringLength);
+
+        (byte[], int) Convert()
+        {
+            var bytes = new byte[byteCount];
+            Encoding.UTF8.GetBytes(chars, 0, stringLength, bytes, 0);
+            return (bytes, stringLength);
+        }
+
+        // simple case, it fits
+        if (byteCount <= maxLength)
+            return Convert();
+
+        var searchLength = -(stringLength >> 1);
+        void Search(ref int a, ref int b)
+        {
+            var lastStringLength = 0;
+            var lastByteCount = 0;
+            while (a > b && searchLength != 0)
+            {
+                lastStringLength = stringLength;
+                lastByteCount = byteCount;
+                stringLength += searchLength;
+                searchLength >>= 1;
+                byteCount = Encoding.UTF8.GetByteCount(chars!, 0, stringLength);
+            }
+
+            if (searchLength != 0 || a > b)
+                return;
+            stringLength = lastStringLength;
+            byteCount = lastByteCount;
+        }
+        do
+        {
+            if (searchLength > 0)
+                Search(ref maxLength, ref byteCount);
+            else
+                Search(ref byteCount, ref maxLength);
+            searchLength = -searchLength;
+        } while (searchLength != 0);
+        
+        return Convert();
+    }
+}
+
+//
+public class StringDataType : DataType<string>
+{
+    public int Read(ReadOnlySpan<byte> data, out string value)
+    {
+        var length = MemoryMarshal.Read<ushort>(data);
+        value = Encoding.UTF8.GetString(data[2..]);
+        return length;
+    }
+
+    public int Write(Span<byte> data, string value)
+    {
+        var (bytes, _) = DataType<string>.ConvertString(this.MaxLength - 2, value);
+
+        var byteCountField = (ushort)bytes.Length;
+        MemoryMarshal.Write(data, ref byteCountField);
+        bytes.CopyTo(data[2..]);
+        return byteCountField;
+    }
+
+    public override int MaxLength { get; }
 }
 
 public abstract class FixedDataType<T> : DataType<T>
 {
-    public override int Length { get; }
-    protected FixedDataType(int length) => this.Length = length;
+    public override int MaxLength { get; }
+    protected FixedDataType(int length) => this.MaxLength = length;
+    public abstract T Read(ReadOnlySpan<byte> data);
+    public abstract void Write(Span<byte> data, ref T value);
 }
 
 public unsafe class IntrinsicDataType<T> : FixedDataType<T> where T : unmanaged
@@ -25,62 +96,16 @@ public unsafe class IntrinsicDataType<T> : FixedDataType<T> where T : unmanaged
     public override void Write(Span<byte> data, ref T value) => MemoryMarshal.Write(data, ref value);
 }
 
-public class StringDataType : FixedDataType<string>
+public class FixedStringDataType : FixedDataType<string>
 {
-    public StringDataType(int length) : base(length) { }
+    public FixedStringDataType(int length) : base(length) { }
 
     public override string Read(ReadOnlySpan<byte> data) => Encoding.UTF8.GetString(data);
 
     public override void Write(Span<byte> data, ref string value)
     {
-        var chars = value.ToCharArray();
-        var stringLength = Math.Min(chars.Length, this.Length);
-        var byteCount = Encoding.UTF8.GetByteCount(chars, 0, stringLength);
-
-        void ConvertAndWrite(Span<byte> dest)
-        {
-            var bytes = new byte[byteCount];
-            Encoding.UTF8.GetBytes(chars, 0, stringLength, bytes, 0);
-            bytes.CopyTo(dest);
-        }
-
-        // simple case, it fits
-        if (byteCount <= this.Length)
-        {
-            ConvertAndWrite(data);
-            return;
-        }
-
-        var searchLength = -(stringLength >> 1);
-        void Search(ref int a, ref int b)
-        {
-            var lastStringLength = 0;
-            var lastByteCount = 0;
-            while (a > b && searchLength != 0)
-            {
-                lastStringLength = stringLength; 
-                lastByteCount = byteCount;
-                stringLength += searchLength;
-                searchLength >>= 1;
-                byteCount = Encoding.UTF8.GetByteCount(chars!, 0, stringLength);
-            }
-
-            if (searchLength != 0 || a > b) 
-                return;
-            stringLength = lastStringLength;
-            byteCount = lastByteCount;
-        }
+        var (bytes, _) = DataType<string>.ConvertString(this.MaxLength, value);
         
-        var targetLength = this.Length;
-        do
-        {
-            if (searchLength > 0)
-                Search(ref targetLength, ref byteCount);
-            else
-                Search(ref byteCount, ref targetLength);
-            searchLength = -searchLength;
-        } while (searchLength != 0);
-        
-        ConvertAndWrite(data);
+        bytes.CopyTo(data);
     }
 }
